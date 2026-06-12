a2e38fc9d51dadaad8a8dad76132d3775302ed1c
Ashish kumar <ashish7371857965@gmail.com>
2026-06-12 18:05:40 +0000

    learning

---
+++ 258.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addDigits(self, num):
+        while num>=10:
+            num=self.operation(num)
+        return num
+
+    def operation(self,num):
+        l=[]
+        while num!=0:
+            digit=num%10
+            l.append(digit)
+            num=num//10
+        return sum(l)
+    
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.addDigits(38))
