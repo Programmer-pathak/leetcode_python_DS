f353e3887db39316dca1ddd36b4cd4f1e5c85b2a
Ashish kumar <ashishkumar2k.ds@gmail.com>
2026-07-19 00:09:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.20%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,35 +10,28 @@ class Solution:
 
         while list1 and list2:
             if list1.val <= list2.val:
-                temp.next = list1  
-                list1 = list1.next 
+                temp.next=ListNode(list1.val)
+                list1=list1.next
+
             else:
-                temp.next = list2  
-                list2 = list2.next 
-            
-            temp = temp.next
-
-         # if list1.val < list2.val:
-                #     temp.next=ListNode(list1.val)
-                #     list1=list1.next
-                #     temp=temp.next
-
-                # if list1.val==list2.val:
-                #     temp.next=ListNode(list1.val)
-                #     list1=list1.next
-                #     temp=temp.next
-                #     temp.next=ListNode(list2.val)
-                #     temp=temp.next
-                #     list2=list2.next
+                temp.next=ListNode(list2.val)
+                list2=list2.next
                 
-                # if list1.val>list2.val:
-                #     temp.next=ListNode(list2.val)
-                #     list2=list2.next
-                #     temp=temp.next
-               
+            temp=temp.next
+
+            
+            # if list1.val <= list2.val:
+            #     temp.next = list1  
+            #     list1 = list1.next 
+            # else:
+            #     temp.next = list2  
+            #     list2 = list2.next 
+            
+            # temp = temp.next
 
         if list1 and not list2:
             temp.next = list1
+
         elif list2 and not list1:
             temp.next = list2
 
