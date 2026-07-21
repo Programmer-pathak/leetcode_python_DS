2916c21a9dfc6f245a3d6bf53bc6b51c691405fd
Ashish kumar <ashishkumar2k.ds@gmail.com>
2026-07-21 12:17:40 +0530

    Time: 14 ms (29.30%) | Memory: 21.1 MB (50.50%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -5,16 +5,13 @@ class Solution:
         right,place=n-1,n-1
         arr=[0]*n
 
-        while left<right:
+        while left<=right:
             if abs(nums[left]) < abs(nums[right]):
                 arr[place]=nums[right]**2
-                place-=1
                 right-=1
             else:
                 arr[place]=nums[left]**2
-                place-=1
                 left+=1
-        if left==right:
-            arr[place]=nums[left]**2
+            place-=1
         
         return arr
