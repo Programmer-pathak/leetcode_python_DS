68c4346ef46990f6cf418fbd508e1aa8d7aa24ad
Ashish kumar <ashishkumar2k.ds@gmail.com>
2026-07-27 23:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (50.27%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,3 +1,26 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2) + int(b, 2))[2:]
+        #return bin(int(a, 2) + int(b, 2))[2:]
+
+        res = []
+        carry = 0
+        i, j = len(a) - 1, len(b) - 1
+
+        # Traverse both strings from right to left
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            
+            # Append remainder (0 or 1) to result
+            res.append(str(total % 2))
+            # Calculate new carry (0 or 1)
+            carry = total // 2
+        
+        # Reverse the array and join into string
+        return "".join(res[::-1])
