26fcb81012eab0baea375f09082b6223a9a5e1fc
Ashish kumar <ashishkumar2k.ds@gmail.com>
2026-07-28 23:55:53 +0530

    Time: 2 ms (73.34%) | Memory: 19.2 MB (57.57%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,11 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        start=0
-        while start<=x:
-            if (start+1)*(start+1) <=x:
-                start+=1
+        if x < 2:
+            return x
+        
+        left, right = 1, x // 2
+        ans = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid <= x:
+                ans = mid      # mid potential answer hai
+                left = mid + 1 # bada square root dhoondhne ke liye aage badhein
             else:
-                return start
-                    
+                right = mid - 1 # square chhota karne ke liye peechhe aayein
+                
+        return ans
 
     
