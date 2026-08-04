033e19362669df8b1dfa1e293dd2a41cc5bd8eaf
Ashish kumar <ashishkumar2k.ds@gmail.com>
2026-08-04 23:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.85%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        mn, mx = min(nums), max(nums)
+        return [x for x in range(mn, mx + 1) if x not in s]
