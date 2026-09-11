3a6717b3c4fd5527e205f1fe076077269f708979
Ashish kumar <ashishkumar2k.ds@gmail.com>
2026-09-11 21:31:34 +0530

    Time: 591 ms (5.05%) | Memory: 19.3 MB (72.53%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -6,14 +6,11 @@ class Solution:
         freq = Counter(digits)
         ans = 0
         
-        # Iterate over all candidate 3-digit even numbers
         for num in range(100, 1000, 2):
             d1, d2, d3 = num // 100, (num // 10) % 10, num % 10
             
-            # Count frequency required for the current number
             req = Counter([d1, d2, d3])
             
-            # Check if available digits satisfy the required counts
             if all(freq[d] >= count for d, count in req.items()):
                 ans += 1
                 
